cb903a9c874f73fce33c923d2ee81ab6a6836d0d
Anshaj Goyal <52253745+leetanshaj@users.noreply.github.com>
2020-07-19 23:09:53 +0530

    Update twitter_creds.py

--- djangoRun/sentimentAnalysis/twitter_creds.py
+++ djangoRun/sentimentAnalysis/twitter_creds.py
@@ -1,5 +1,5 @@
 global consumer_key,consumer_secret,access_token_key,access_token_secret
-consumer_key='bYa4JD9WSamEVABRbxHaUzTHJ'
-consumer_secret='byGpIOUTbMuikrOT7DGaJWbHzWK4PO9IyQc4wo4s2I3tMJnZeE'
-access_token_key='4844792894-p6HZrNoGcFhJnr5aHOwNnpj8XOM2yiKKReYmO8J'
-access_token_secret='9nk4Z6bee0IR9dqOr9Pf0gXTRhYdtzrOMt0zdwZdLIOld'
+consumer_key='bYa4JD9WSamEVABRb********'
+consumer_secret='byGpIOUTbMuikrOT7DGaJWbHzWK4PO9IyQc4*********'
+access_token_key='4844792894-p6HZrNoGcFhJnr5aHOwNnpj8XOM**********'
+access_token_secret='9nk4Z6bee0IR9dqOr9Pf0gXTRhYdtzrOM*********'
